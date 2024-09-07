153ff9052aabb59cc3ed348aa50af4c0be00398f
Adit0507 <adi05072002@gmail.com>
2024-09-08 00:46:57 +0530

    infix operators

--- ast/ast.go
+++ ast/ast.go
@@ -121,8 +121,8 @@ type PrefixExpression struct {
 	Right    Expression
 }
 
-func (pe *PrefixExpression) expressionNode() {}
-func (pe *PrefixExpression) TokenLiteral() string  {return pe.Token.Literal}
+func (pe *PrefixExpression) expressionNode()      {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
 func (pe *PrefixExpression) String() string {
 	var out bytes.Buffer
 
@@ -130,5 +130,26 @@ func (pe *PrefixExpression) String() string {
 	out.WriteString(pe.Operator)
 	out.WriteString(pe.Right.String())
 	out.WriteString(")")
+	return out.String()
+}
+
+type InfixExpression struct {
+	Token    token.Token
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (ie *InfixExpression) expressionNode()      {}
+func (ie *InfixExpression) TokenLiteral() string {return ie.Token.Literal}
+func (ie *InfixExpression) String() string{
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(" "+ ie.Operator + " ")
+	out.WriteString(ie.Right.String())
+	out.WriteString(")")
+
 	return out.String()
 }
